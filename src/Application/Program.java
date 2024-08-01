9320146380f6de7a38824f0504f40d1d847a441f
pedrobarboz4 <pedro.bv1@puccampinas.edu.br>
2024-07-31 22:23:34 -0300

    First Class Position

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,10 +1,14 @@
 package Application;
 
+import Boardgame.Position;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Program {
     public static void main(String[] args) {
 
-        
+        Position pos = new Position(3,5);
+
+        System.out.println(pos);
     }
 }
